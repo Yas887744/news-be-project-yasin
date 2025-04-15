cfa0738839faeaa8a6e93f9b4f769fa2b8e0cdfd
Yas887744 <yasin.rashid@outlook.com>
2025-04-15 17:14:00 +0100

    completed task 3 seeding-insert the data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,6 @@
 const db = require("../connection")
-
+const format = require("pg-format")
+const { convertTimestampToDate } = require("./utils.js")
 const seed = ({ topicData, userData, articleData, commentData }) => {
   return db
     .query(`DROP TABLE IF EXISTS comments`)
@@ -47,6 +48,56 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
       author VARCHAR(250),
       FOREIGN KEY(author) REFERENCES users(username),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`) 
+    })
+    .then(() => {
+      //console.log(topicData, "topic data")
+      const formattedTopicData = topicData.map((topics) => {
+        return [topics.slug, topics.description, topics.img_url]
+      })
+      //console.log(formattedTopicData, "<<<<<<")
+      const insertTopicData = format(`INSERT INTO topics(slug, description, img_url)
+        VALUES %L`, formattedTopicData)
+      return db.query(insertTopicData)
+    })
+    .then(() => {
+      const formattedUsersData = userData.map((users) => {
+        return [users.username, users.name, users.avatar_url]
+      })
+      const insertUserData = format(`INSERT INTO users(username, name, avatar_url) 
+        VALUES %L`, formattedUsersData)
+        return db.query(insertUserData)
+    })
+    .then(() => {
+      const formattedArticlesData = articleData.map((articles) => {
+        return [
+          articles.title,
+          articles.topic,
+          articles.author, 
+          articles.body,
+          articles.votes,
+          articles.article_img_url
+        ]
+      })
+      const insertArticleData = format(`INSERT INTO articles(title, topic, author, body, votes, article_img_url)
+        VALUES %L`, formattedArticlesData)
+      return db.query(insertArticleData)
+    })
+    .then(() => {
+      const commentDataCorrectTime = commentData.map((comments) => convertTimestampToDate(comments))
+      const formattedCommentsData = commentDataCorrectTime.map((comments) => {
+        return [
+          comments.body, 
+          comments.article_id, 
+          comments.author, 
+          comments.votes,
+          comments.created_at
+        ]
+      })
+      //console.log(formattedCommentsData, "formatted comments data")
+      const insertCommentData = format(`INSERT INTO comments(body, article_id, author, votes, created_at)
+        VALUES %L`, formattedCommentsData)
+
+      return db.query(insertCommentData)
     })
 };
 module.exports = seed;
