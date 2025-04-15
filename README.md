3740aa24312565a68199e02ceab9be6534af776a
Yas887744 <yasin.rashid@outlook.com>
2025-04-15 15:02:06 +0100

    setup and task 1 and 2 complete

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+-Create a .env.test file and a .env.development file in the main part of the directory.
+-Inside the .env.test file write PGDATABASE = nc_news_test
+-Inside the .env.development file write PGDATABASE = nc_news
+-This will allow you run my files and connect the the database successfully.
+-You can test that these run correctly and you are in the corect databse by typing npm run test-seed and npm run seed-dev. You should see that it will say in the terminal connected to "the correct database".
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,52 @@
 const db = require("../connection")
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db
+    .query(`DROP TABLE IF EXISTS comments`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS articles`)
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users`)
+    }) 
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS topics`)
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE topics(
+      slug VARCHAR(100) PRIMARY KEY,
+      description VARCHAR(500) NOT NULL,
+      img_url VARCHAR(1000) NOT NULL);`)
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE users(
+      username VARCHAR(250) PRIMARY KEY,
+      name VARCHAR(500) NOT NULL,
+      avatar_url VARCHAR(1000) NOT NULL);`)
+    }) 
+    .then(() => {
+      return db.query(`CREATE TABLE articles(
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR(200),
+      topic VARCHAR(100),
+      FOREIGN KEY (topic) REFERENCES topics(slug),
+      author VARCHAR(250),
+      FOREIGN KEY (author) REFERENCES users(username),
+      body TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      votes INT DEFAULT 0, 
+      article_img_url VARCHAR(1000));`)
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE comments(
+      comment_id SERIAL PRIMARY KEY,
+      article_id INT, 
+      FOREIGN KEY (article_id) REFERENCES articles(article_id),
+      body TEXT,
+      votes INT DEFAULT 0,
+      author VARCHAR(250),
+      FOREIGN KEY(author) REFERENCES users(username),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`) 
+    })
 };
 module.exports = seed;
