98b103e41a7b72c55f979deba891d1acc1ba34e1
Yas887744 <yasin.rashid@outlook.com>
2025-04-29 12:31:15 +0100

    tast 3 get article by article_id with error handling completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,6 +39,25 @@ describe("GET /api/topics", () => {
       })
   })
 })
+describe("GET /api/articles/:article_id", () => {
+  test("200: responds with article object to respective article_id", () => {
+    return request(app)
+    .get("/api/articles/1")
+    .expect(200)
+    .then(({body}) => {
+      expect(body.article).toEqual({
+        article_id: 1,
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: '2020-07-09T20:11:00.000Z',
+        votes: 100,
+        article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+      })
+    })
+  })
+})
 describe("Error Handling", () => {
   test("404: accessing a non-existant route", () => {
     return request(app)
@@ -46,6 +65,22 @@ describe("Error Handling", () => {
     .expect(404)
     .then((response) => {
       expect(response.body).toEqual({msg: "Error Not Found"})
+    })
+  })
+  test("404: When passed an article_id that doesn't exist", () => {
+    return request(app)
+    .get("/api/articles/1000")
+    .expect(404)
+    .then(({body}) => {
+      expect(body).toEqual({msg: "No article found under article_id: 1000"})
+    })
+  })
+  test("400: invalid article_id input", () => {
+    return request(app)
+    .get("/api/articles/hello")
+    .expect(400)
+    .then(({body}) => {
+      expect(body).toEqual({msg: "Invalid input"})
     })
   })
 })
