7e8d756296331e79fc663e835bff07bd8d70e447
Yas887744 <yasin.rashid@outlook.com>
2025-05-02 16:42:46 +0100

    hosting setup

--- db/connection.js
+++ db/connection.js
@@ -4,13 +4,21 @@ const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool();
+const config = {};
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+const db = new Pool(config);
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set")
 } else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
 
 
-module.exports = db;
+
+module.exports = db
