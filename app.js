721b4f3118cdf1f6de70faf0306968b2fda5801e
Yas887744 <yasin.rashid@outlook.com>
2025-05-19 09:21:50 +0100

    added cors to app.js

--- app.js
+++ app.js
@@ -1,49 +1,60 @@
-const express = require('express')
-const app = express()
-const {getApi, getTopics, getArticleById, getArticles, getComments, postComment, patchArticle, deleteComment, getUsers} = require('./controller')
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const {
+  getApi,
+  getTopics,
+  getArticleById,
+  getArticles,
+  getComments,
+  postComment,
+  patchArticle,
+  deleteComment,
+  getUsers,
+} = require("./controller");
+app.use(express.json());
 
-app.get("/api", getApi)
-app.get("/api/topics", getTopics)
-app.get("/api/articles/:article_id", getArticleById)
-app.get("/api/articles", getArticles)
-app.get("/api/articles/:article_id/comments", getComments)
-app.post("/api/articles/:article_id/comments", postComment)
-app.patch("/api/articles/:article_id", patchArticle)
-app.delete("/api/comments/:comment_id", deleteComment)
-app.get("/api/users", getUsers)
+app.get("/api", getApi);
+app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id/comments", getComments);
+app.post("/api/articles/:article_id/comments", postComment);
+app.patch("/api/articles/:article_id", patchArticle);
+app.delete("/api/comments/:comment_id", deleteComment);
+app.get("/api/users", getUsers);
 
-
+app.use(cors());
 app.use((err, req, res, next) => {
-    if(err.status && err.msg){
-        res.status(err.status).send({msg: err.msg})
-    }else{
-        next(err)
-    }
-})
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
 app.use((err, req, res, next) => {
-    if(err.code === "22P02"){
-        res.status(400).send({msg: "Invalid input"})
-    }else{
-        next(err)
-    }
-})
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid input" });
+  } else {
+    next(err);
+  }
+});
 app.use((err, req, res, next) => {
-    if(err.code === "23503"){
-        res.status(400).send({msg: "Username invalid"})
-    }else{
-        next(err)
-    }
-})
+  if (err.code === "23503") {
+    res.status(400).send({ msg: "Username invalid" });
+  } else {
+    next(err);
+  }
+});
 app.use((err, req, res, next) => {
-    if(err.code === "23502"){
-        res.status(400).send({msg: "Invalid: No input"})
-    }else{
-        next(err)
-    }
-})
+  if (err.code === "23502") {
+    res.status(400).send({ msg: "Invalid: No input" });
+  } else {
+    next(err);
+  }
+});
 app.all("/*splat", (req, res) => {
-    res.status(404).send({msg: "Error Not Found"})
-})
+  res.status(404).send({ msg: "Error Not Found" });
+});
 
-module.exports = app
+module.exports = app;
